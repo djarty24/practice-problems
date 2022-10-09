97a0d13a59b1782c2df4def69a45c99d841b32d5
djarty24 <24revati@gmail.com>
2022-10-09 16:44:48 -0700

    Created a window for the game to be displayed on.

--- Stormtrooper/src/Main.java
+++ Stormtrooper/src/Main.java
@@ -1,7 +1,16 @@
 
+import javax.swing.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		JFrame window = new JFrame("Game");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(500, 500);
+		window.setLocation(450, 150);
+		window.setVisible(true);
+		
 	}
+	
 }
